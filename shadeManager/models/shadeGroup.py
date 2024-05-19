dd42565b55910012f065c9c34d4a4643eb6318fe
Bryce Ashey <bryce@geekdojo.com>
2024-05-19 08:02:34 -0700

    fixed typo

--- shadeManager/models/shadeGroup.py
+++ shadeManager/models/shadeGroup.py
@@ -15,9 +15,9 @@ class ShadeGroup():
         shadeGroups = dict([
             ("Dining", ShadeGroup("192.168.201.20", "Dining", ["FLI", "EBH"])),
             ("Kitchen", ShadeGroup("192.168.201.20", "Kitchen", ["YQP"])),
-            ("LivingEast"), ShadeGroup("192.168.201.20", "LivingEast", ["LZU","TOA","RXG"]),
-            ("LivingSouth"), ShadeGroup("192.168.201.20", "LivingSouth", ["ATR"]),
-            ("BalconyDoor"), ShadeGroup("192.168.201.20", "BalconyDoor", ["KTB"]),
+            ("LivingEast", ShadeGroup("192.168.201.20", "LivingEast", ["LZU","TOA","RXG"])),
+            ("LivingSouth", ShadeGroup("192.168.201.20", "LivingSouth", ["ATR"])),
+            ("BalconyDoor", ShadeGroup("192.168.201.20", "BalconyDoor", ["KTB"])),
             ("Primary", ShadeGroup("192.168.201.20", "Primary", ["KXJ"])),
             ("Studio", ShadeGroup("192.168.201.20", "Studio", ["NKK", "CYV"]))
         ])
