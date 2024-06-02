9eede5b010e01935800784884308cdc8c273b3e5
Bryce Ashey <bryce@geekdojo.com>
2024-06-02 09:15:25 -0700

    added an 'All' group

--- shadeManager/models/shadeGroup.py
+++ shadeManager/models/shadeGroup.py
@@ -13,13 +13,14 @@ class ShadeGroup():
     @staticmethod
     def Populate():
         shadeGroups = dict([
+            ("All", ShadeGroup("192.168.201.20", "All", ["FLI", "EBH", "YQP", "LZU", "TOA", "RXG", "ATR", "KTB", "KXJ", "NKK", "CYV"])),
             ("Dining", ShadeGroup("192.168.201.20", "Dining", ["FLI", "EBH"])),
             ("Kitchen", ShadeGroup("192.168.201.20", "Kitchen", ["YQP"])),
             ("LivingEast", ShadeGroup("192.168.201.20", "LivingEast", ["LZU","TOA","RXG"])),
             ("LivingSouth", ShadeGroup("192.168.201.20", "LivingSouth", ["ATR"])),
             ("BalconyDoor", ShadeGroup("192.168.201.20", "BalconyDoor", ["KTB"])),
             ("Primary", ShadeGroup("192.168.201.20", "Primary", ["KXJ"])),
-            ("Studio", ShadeGroup("192.168.201.20", "Studio", ["NKK", "CYV"]))
+            ("Studio", ShadeGroup("192.168.201.20", "Studio", ["NKK", "CYV"]))            
         ])
 
         return shadeGroups
