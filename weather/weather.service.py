2a72121277138cff773075c6a5614598d300befb
Bryce Ashey <bryce@geekdojo.com>
2025-05-22 10:01:38 -0700

    added some error handling to weather

--- weather/weather.service.py
+++ weather/weather.service.py
@@ -10,24 +10,34 @@ _logger.setLevel(logging.DEBUG)
 _handler = logging.handlers.SysLogHandler(address = '/dev/log')
 _logger.addHandler(_handler)
 
+_URL = f"https://api.open-meteo.com/v1/forecast?latitude=32.711&longitude=-117.155&current=temperature_2m,relative_humidity_2m,wind_speed_10m,rain,cloud_cover&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch"
+
 _UDP_IP = "192.168.201.21"
-_UDP_PORT = 7001
-_UDP_MESSAGE = ""
-_URL = f"https://api.open-meteo.com/v1/forecast?latitude=32.711&longitude=-117.155&current=temperature_2m&temperature_unit=fahrenheit"
+_UDP_PORT_TEMPERATURE = 7001
+
+def sendUdp(msg, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+    sock.sendto(msg.encode(), (_UDP_IP, port))
+    sock.close()
 
 if __name__=='__main__':
     _logger.debug("Monitoring weather...")
 
     while True:
-        response = requests.get(_URL)
-        weather_data = json.loads(response.text)
-        _UDP_MESSAGE = str(weather_data["current"]["temperature_2m"])
-        _logger.debug(f"Current outside temp:{_UDP_MESSAGE}")
-
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
-        sock.sendto(_UDP_MESSAGE.encode(), (_UDP_IP, _UDP_PORT))
-        sock.close()
-            
+        try:
+            response = requests.get(_URL)
+            weather_data = json.loads(response.text)
+            temperature = str(weather_data["current"]["temperature_2m"])
+            humidity = str(weather_data["current"]["relative_humidity_2m"])
+            wind_speed = str(weather_data["current"]["wind_speed_10m"])
+            rain = str(weather_data["current"]["rain"])
+            _logger.debug(f"Current outside temp:{temperature}")
+        except Exception as e:
+            _logger.error(f"Error fetching weather data: {e}")
+            time.sleep(900)
+            continue
+        
         time.sleep(900)
+
         
 _logger.debug("Weather monitor closing...")
